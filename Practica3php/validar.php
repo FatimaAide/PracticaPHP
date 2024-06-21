7078e8aa11575db745e6d765f2c6820cdcabb049
FatimaAide <hervertfatima47@gmail.com>
2024-06-20 22:47:03 -0600

    COMMIT

--- Practica3php/validar.php
+++ Practica3php/validar.php
@@ -7,7 +7,7 @@ function Ticket($nombre, $altura) {
         <head>
             <meta charset='UTF-8'>
             <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-            <title>Ticket para Montaña Rusa</title>
+            <title>Ticket</title>
             <style>
                 body {
                     font-family:cursive;
